1cc6fa21cc51f67785eb445f56484ab92c1df4f9
Anatoliy Maximichev <maximichev@mail.ru>
2021-06-22 14:52:27 +0300

    Добавил в 5 задачу флаг, что бы слова не повторялись

--- Task_5.py
+++ Task_5.py
@@ -5,14 +5,18 @@ adverbs = ["сегодня", "вчера", "завтра", "позавчера",
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
 
-def get_jokes(n):
-    for _ in range(n):
+def get_jokes(number_joke, flag):
+    for _ in range(number_joke):
         first_word = choice(nouns)
         second_word = choice(adverbs)
         third_word = choice(adjectives)
         joke = f'{first_word} {second_word} {third_word}'
+        if flag:
+            nouns.remove(first_word)
+            adverbs.remove(second_word)
+            adjectives.remove(third_word)
         print(joke)
 
 
-index = int(input('Введите количество шуток: '))
-get_jokes(index)
+index = int(input('Введите количество шуток (1-5): '))
+get_jokes(index, True)
